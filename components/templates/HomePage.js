a539ae973442079e4eaca5bec633862a5c5cc92e
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2023-02-25 21:11:40 +0330

    final commit

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -3,9 +3,15 @@ import CustomerCard from "../modules/CustomerCard";
 const HomePage = ({ customers }) => {
   return (
     <div className="flex flex-col items-center justify-center gap-16 lg:flex-row lg:flex-wrap">
-      {customers.map((customer) => (
-        <CustomerCard {...customer} key={customer._id} />
-      ))}
+      {customers.length ? (
+        customers.map((customer) => (
+          <CustomerCard {...customer} key={customer._id} />
+        ))
+      ) : (
+        <h2 className="my-64 text-center text-4xl text-slate-400">
+          Nothing To Show here
+        </h2>
+      )}
     </div>
   );
 };
